2dae7fedc845f6b0e185990ff89e13181f457a2a
yunaKim01 <peminsyn@gmail.com>
2019-09-27 18:02:51 +0900

    Initial commit

---
+++ src/hello/HelloJava.java
@@ -0,0 +1,17 @@
+/*
+ * Date: 2019년 8월 28일
+ * Author: 김윤지
+ * Description: 첫 번째 자바 출력 연습입니다
+ * Version: 1.0
+ */
+package hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// 한 줄 출력
+		System.out.println("Hello Java");
+
+	}
+
+}
